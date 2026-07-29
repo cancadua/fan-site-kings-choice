851f8b811fcdfa7f1e0d8c3cf4da5d5d555f733c
Adrian <cancadua@protonmail.com>
2026-07-29 22:45:31 +0200

    refactor: update home component to use extracted shared components and optimize styles

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { HomeEventsHighlightComponent } from '../../shared/home-events-highlight/home-events-highlight.component';
+import { HomeCtaSectionComponent } from '../../shared/home-cta-section/home-cta-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, HomeEventsHighlightComponent, HomeCtaSectionComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
