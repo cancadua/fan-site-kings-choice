7132cb1f9a7105c9f1eb7c337bd0f4cd696845b6
Adrian <cancadua@protonmail.com>
2026-09-01 22:17:55 +0200

    refactor: rebuild Struggle for Supremacy on shared event-content components

--- src/app/features/events/struggle-for-supremacy/struggle-for-supremacy.component.ts
+++ src/app/features/events/struggle-for-supremacy/struggle-for-supremacy.component.ts
@@ -3,11 +3,12 @@ import { AppTabsComponent } from '../../../shared/app-tabs/app-tabs.component';
 import { TabbedPage } from '../../../shared/app-tabs/tabbed-page';
 import { StruggleForSupremacyTabs } from './struggle-for-supremacy.schema';
 import { WarriorSceneComponent } from '../../../shared/warrior-scene/warrior-scene.component';
+import { EVENT_CONTENT } from '../../../shared/event-content';
 
 @Component({
   selector: 'app-struggle-for-supremacy',
   standalone: true,
-  imports: [AppTabsComponent, WarriorSceneComponent],
+  imports: [AppTabsComponent, WarriorSceneComponent, ...EVENT_CONTENT],
   templateUrl: './struggle-for-supremacy.component.html',
   styleUrl: './struggle-for-supremacy.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
