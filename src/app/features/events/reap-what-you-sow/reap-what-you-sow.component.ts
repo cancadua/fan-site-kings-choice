3876f9435b9b01c6202d81783c86e6f55f12ec37
Adrian <cancadua@protonmail.com>
2026-09-01 22:17:55 +0200

    refactor: rebuild Reap What You Sow on shared event-content components

--- src/app/features/events/reap-what-you-sow/reap-what-you-sow.component.ts
+++ src/app/features/events/reap-what-you-sow/reap-what-you-sow.component.ts
@@ -2,11 +2,12 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { AppTabsComponent } from '../../../shared/app-tabs/app-tabs.component';
 import { TabbedPage } from '../../../shared/app-tabs/tabbed-page';
 import { ReapWhatYouSowTabs } from './reap-what-you-sow.schema';
+import { EVENT_CONTENT } from '../../../shared/event-content';
 
 @Component({
   selector: 'app-reap-what-you-sow',
   standalone: true,
-  imports: [AppTabsComponent],
+  imports: [AppTabsComponent, ...EVENT_CONTENT],
   templateUrl: './reap-what-you-sow.component.html',
   styleUrl: './reap-what-you-sow.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
